679edc72681ade73ef6e046d02901ca378185220
ParellyAkhil07 <161436763+ParellyAkhil07@users.noreply.github.com>
2024-08-02 15:32:10 +0530

    Add new Java unit test cases

---
+++ output/src/main/java/io/buildpacks/example/sample/SampleApplicationTest.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+public class SampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
