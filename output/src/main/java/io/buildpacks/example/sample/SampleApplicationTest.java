6afab432cd71467454ed602eabd43d0f368ec460
ParellyAkhil07 <161436763+ParellyAkhil07@users.noreply.github.com>
2024-08-02 15:42:36 +0530

    Delete output directory

--- output/src/main/java/io/buildpacks/example/sample/SampleApplicationTest.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-
-@SpringBootTest(webEnvironment = WebEnvironment.NONE)
-public class SampleApplicationTests {
-
-    @Test
-    public void contextLoads() {
-    }
