64507565655ca537e498334770986c20ace3dd19
ParellyAkhil07 <161436763+ParellyAkhil07@users.noreply.github.com>
2024-08-02 15:18:22 +0530

    Add new Java unit test cases

---
+++ output/src/test/java/io/buildpacks/example/sample/SampleApplicationTests.java
@@ -0,0 +1,13 @@
+import io.buildpacks.example.sample.SampleApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SampleApplication.class)
+public class SampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
