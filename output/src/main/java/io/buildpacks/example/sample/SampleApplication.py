4a02f45a91a99d22d959d7afbf6b8da531e93be0
ParellyAkhil07 <161436763+ParellyAkhil07@users.noreply.github.com>
2024-08-02 15:12:51 +0530

    Add new Python file

---
+++ output/src/main/java/io/buildpacks/example/sample/SampleApplication.py
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SampleApplicationTest {
+
+    @Test
+    public void testMain() {
+        String[] args = new String[] {"arg1", "arg2"};
+        SampleApplication.main(args);
+        // Assert that the main method runs successfully
+    }
