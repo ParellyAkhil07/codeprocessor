03ef8eba51c82cd35099f58c75136042ed94f74e
ParellyAkhil07 <161436763+ParellyAkhil07@users.noreply.github.com>
2025-01-29 15:58:03 +0530

    Converted Java to Python

---
+++ output/ch09/Exercise.py
@@ -0,0 +1,24 @@
+# Converted from Java to Python
+
+/**
+ * Exercise on encapsulation and generalization.
+ */
+public class Exercise {
+
+    public static void main(String[] args) {
+        String s = "((3 + 7) * 2)";
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                count++;
+            } else if (c == ')') {
+                count--;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+}
